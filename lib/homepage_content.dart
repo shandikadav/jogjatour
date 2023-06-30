3eabdeddacbaa4eb44c2da40dfbb4125f3a8eda4
shandikadav <shandikadavid.ardiansyah@students.amikom.ac.id>
2023-06-30 19:23:16 +0700

    fifth commit

--- lib/homepage_content.dart
+++ lib/homepage_content.dart
@@ -1,21 +1,43 @@
 class HomepageContents {
   String img;
   String title;
+  String desc;
+  String addr;
 
-  HomepageContents({required this.img, required this.title});
+  HomepageContents(
+      {required this.img,
+      required this.title,
+      required this.desc,
+      required this.addr});
 }
 
 List<HomepageContents> contents = [
   HomepageContents(
-    img: 'https://images.unsplash.com/photo-1631002165167-8fe92607c00e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80', 
-    title: 'Jalan Malioboro'),
-  
-  HomepageContents(img: 'https://images.unsplash.com/photo-1566559631133-969041fc5583?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80',
-   title: 'Candi Prambanan'),
-
-   HomepageContents(img: 'https://images.unsplash.com/photo-1630214801769-24784bfd2b9c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=871&q=80', 
-   title: 'Taman Sari'),
-
-   HomepageContents(img: 'https://images.unsplash.com/flagged/photo-1571588896019-ac4b46e88edb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80', 
-   title: 'Pantai Parangtritis')
+      img:
+          'https://images.unsplash.com/photo-1631002165145-f6672bfa9672?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
+      title: 'Jalan Malioboro',
+      desc:
+          'Jalan Malioboro adalah salah satu jalan utama dan ikonik di Yogyakarta. Jalan ini terkenal sebagai pusat perbelanjaan, wisata, dan kegiatan budaya di kota ini. Jalan Malioboro memanjang sekitar 2,5 kilometer dari Tugu Yogyakarta hingga Keraton Yogyakarta. Jalan ini terdiri dari dua jalur yang dipisahkan oleh trotoar yang ramai dengan pedagang kaki lima, toko-toko, hotel, restoran, dan pusat perbelanjaan.',
+      addr: 'Sosromenduran, Gedong Tengen, Kota Yogyakarta, Daerah Istimewa Yogyakarta'),
+  HomepageContents(
+      img:
+          'https://images.unsplash.com/photo-1578469550956-0e16b69c6a3d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=806&q=80',
+      title: 'Candi Prambanan',
+      desc:
+          'Candi Prambanan adalah kompleks candi Hindu terbesar di Indonesia dan merupakan Situs Warisan Dunia UNESCO. Candi ini dibangun pada abad ke-9 oleh dinasti Sanjaya dan merupakan contoh arsitektur Hindu klasik yang megah. Candi Prambanan terkenal karena kemegahan dan keindahan relief-reliefnya serta menjadi salah satu tujuan wisata utama di Yogyakarta.',
+      addr: 'Jl. Raya Solo - Yogyakarta No. 16, Prambanan, Sleman, Yogyakarta'),
+  HomepageContents(
+      img:
+          'https://images.unsplash.com/photo-1630214801769-24784bfd2b9c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=871&q=80',
+      title: 'Taman Sari',
+      desc:
+          'Taman Sari adalah bekas kompleks taman dan kolam renang dari Keraton Yogyakarta, istana resmi Sultan Yogyakarta. Kompleks ini dibangun pada abad ke-18 sebagai tempat rekreasi dan tempat peristirahatan bagi keluarga kerajaan. Taman Sari memiliki nilai sejarah yang tinggi dan merupakan salah satu objek wisata terkenal di Yogyakarta.',
+      addr: 'Jl. Tamanan, Patehan, Kraton, Yogyakarta.'),
+  HomepageContents(
+      img:
+          'https://images.unsplash.com/photo-1629605924917-d2de56a40951?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
+      title: 'Pantai Parangtritis',
+      desc:
+          'Pantai Parangtritis adalah salah satu pantai terkenal di Yogyakarta dan merupakan destinasi wisata populer di wilayah tersebut. Pantai ini terletak sekitar 27 kilometer selatan Yogyakarta dan dikenal karena keindahan pemandangannya, pasir hitamnya yang unik, serta pesona olahraga selancar.',
+      addr: 'Jl. Parangtritis, Kretek, Bantul, Yogyakarta.')
 ];
--- lib/pages/main_tour.dart
+++ lib/pages/main_tour.dart
@@ -3,20 +3,17 @@ import 'package:flutter/services.dart';
 import 'package:jogjatour/pages/homepages.dart';
 import 'package:jogjatour/homepage_content.dart';
 
-
 Widget validateData(String input) {
   if (input != dataHihihiha) {
     return Scaffold(
       body: Center(child: Text("Data tidak ada!")),
     );
   } else {
-    List<HomepageContents> filteredContents = contents
-        .where((data) => data.title == input)
-        .toList();
+    List<HomepageContents> filteredContents =
+        contents.where((data) => data.title == input).toList();
 
     if (filteredContents.isEmpty) {
       return AnnotatedRegion<SystemUiOverlayStyle>(
-        
         value: SystemUiOverlayStyle(
           statusBarColor: Colors.transparent,
         ),
@@ -27,38 +24,69 @@ Widget validateData(String input) {
     } else {
       HomepageContents selectedContent = filteredContents.first;
       return Scaffold(
-        backgroundColor: Color.fromARGB(245, 248, 255, 255),
+        backgroundColor: Color.fromARGB(245, 10, 77, 104),
         body: Column(
           children: [
             Stack(
-              children: [ClipRRect(
-                child: Image.network(selectedContent.img,
-                width: double.infinity,
-                height: 245,
-                fit: BoxFit.cover,),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 215),
-                child: Container(
-                height: 425,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(35), topRight: Radius.circular(35)),
-                  color: Color.fromARGB(245, 248, 255, 255),
+              children: [
+                ClipRRect(
+                  child: Image.network(
+                    selectedContent.img,
+                    width: double.infinity,
+                    height: 245,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(30),
-                  child: Text(selectedContent.title,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500
-                  ),),
-                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 215),
+                  child: Container(
+                    height: 425,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(35),
+                          topRight: Radius.circular(35)),
+                      color: Color.fromARGB(245, 10, 77, 104),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(25),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                selectedContent.title,
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.w700,
+                                    color: Color.fromARGB(245, 245, 248, 255)),
+                              ),
+                              Icon(Icons.favorite_border, color: Color.fromARGB(100, 245, 248, 255),)
+                            ],
                           ),
-              )
+                          SizedBox(height: 5,),
+                          Text(selectedContent.addr,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 9,
+                            fontWeight: FontWeight.w300,
+                            color: Color.fromARGB(245, 245, 248, 255)
+                          ),),
+                          SizedBox(height: 30,),
+                          Row(
+                            children: [
+                              Text("Deskripsi Wisata", style: TextStyle(fontWeight: FontWeight.w700, color: Color.fromARGB(245, 245, 248, 255)),),
+                            ],
+                          ),
+                          SizedBox(height: 15,),
+                          Text(selectedContent.desc, textAlign: TextAlign.justify,style: TextStyle(fontWeight: FontWeight.w300, fontSize: 13, color: Color.fromARGB(245, 245, 248, 255)),)
+                        ],
+                      ),
+                    ),
+                  ),
+                )
               ],
             ),
-            
           ],
         ),
       );
